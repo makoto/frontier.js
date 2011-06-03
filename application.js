0b1a0572b1959a8ed5e883aaace4487bb33863f6
Makoto Inoue <inouemak@googlemail.com>
2011-06-03 17:26:07 +0100

    Refactoring

--- application.js
+++ application.js
@@ -2,11 +2,14 @@
 // 
 $(document).ready(function(){
   PathFinder.init({
+    collection:"html",  // Optional
+    events:["click", "mousemove"],  // Optional
     custom:{
       session_id:123
     }
   });
-  $(document).bind("PathFinder.clicked", function(e, result){
+  $(document).bind("PathFinder.all", function(e, result){
     console.log(JSON.stringify(result));
   });
+  
 });
--- path_finder.js
+++ path_finder.js
@@ -1,100 +1,84 @@
+PathFinder = (function(){
 
-PathFinder = {}
-PathFinder.init = function(opts){
-  if (!opts.container)
-    container = document;
+  var PathFinder = {}
 
-  $(container).click(
-    function(e){
-      var result = {
-       time: new Date,
-       event_name:"click",
-       text:PathFinder.getText(e.target),
-       path:PathFinder.getElementCSSPath(e.target),
-       url: window.location.pathname,
+  PathFinder.init = function(opts){
+    if (!opts.container)
+      container = document;
+
+    var events = [];
+    if (!opts.events) {
+      events =  ["click"]
+    }else if (opts.events == "all" || opts.events == ["all"]){
+      events =  ["blur", "focus", "focusin", "focusout", "load", "resize", "scroll", "unload", "click", "dblclick", "mousedown", "mouseup", "mousemove", "mouseover", "mouseout", "mouseenter", "mouseleave", "change", "select", "submit", "keydown", "keypress", "keyup", "error"] 
+    }else{
+      events = opts.events
+    };
+
+    events.forEach(function(event_name){
+      $(container).bind(event_name,
+        function(e){
+          console.log(event_name);
+          var result = {
+           time: new Date,
+           event_name:event_name,
+           text: getText(e.target),
+           path: getElementCSSPath(e.target),
+           url: window.location.pathname,
+          }
+          console.log(result);
+          if (opts.custom)
+            result.custom = opts.custom;
+
+          $(this).trigger("PathFinder." + event_name, result)
+          $(this).trigger("PathFinder.all", result)
+        }
+      );  
+    })
+  }
+
+  function getText(e) {
+    var text = null;
+    if ($(e).text()){
+      // Filter out child elements. nodeType 3 means text.
+      text = $(e).contents().filter(function(){return this.nodeType === 3}).text().trim()
+    }else if ($(e).attr("value")){
+      text = $(e).attr("value");
+    }else{
+      text = "ERROR"
+    }
+    return text;
+  }
+
+  // getElementCSSPath, getElementCSSSelector are taken from
+  // http://code.google.com/p/fbug/source/browse/branches/firebug1.6/content/firebug/lib.js
+  function getElementCSSSelector(element)
+  {
+      if (!element || !element.localName)
+          return "null";
+
+      var label = element.localName.toLowerCase();
+      if (element.id)
+          label += "#" + element.id;
+
+      if (element.classList && element.classList.length > 0)
+          label += "." + element.classList.item(0);
+
+      return label;
+  };
+
+  function getElementCSSPath(element)
+  {   
+      var paths = [];
+
+      for (; element && element.nodeType == 1; element = element.parentNode)
+      {
+          var selector = getElementCSSSelector(element);
+          paths.splice(0, 0, selector);
       }
 
-      if (opts.custom)
-        result.custom = opts.custom;
+      return paths.length ? paths.join(" ") : null;
+  };
 
-      $(this).trigger("PathFinder.clicked", result)
-    }
-  );  
-}
-
-PathFinder.getText = function(e) {
-  var text = null;
-  if ($(e).text()){
-    // Filter out child elements. nodeType 3 means text.
-    text = $(e).contents().filter(function(){return this.nodeType === 3}).text().trim()
-  }else if ($(e).attr("value")){
-    text = $(e).attr("value");
-  }else{
-    text = "ERROR"
-  }
-  return text;
-}
-
-// getElementXPath, getElementTreeXPath, getElementCSSPath, getElementCSSSelector are taken from
-// http://code.google.com/p/fbug/source/browse/branches/firebug1.6/content/firebug/lib.js
-PathFinder.getElementXPath = function(element)
-{
-    var getElementTreeXPath = function(element)
-    {
-        var paths = [];
-
-        // Use nodeName (instead of localName) so namespace prefix is included (if any).
-        for (; element && element.nodeType == 1; element = element.parentNode)
-        {
-            var index = 0;
-            for (var sibling = element.previousSibling; sibling; sibling = sibling.previousSibling)
-            {
-                // Ignore document type declaration.
-                if (sibling.nodeType == Node.DOCUMENT_TYPE_NODE)
-                    continue;
-
-                if (sibling.nodeName == element.nodeName)
-                    ++index;
-            }
-
-            var tagName = element.nodeName.toLowerCase();
-            var pathIndex = (index ? "[" + (index+1) + "]" : "");
-            paths.splice(0, 0, tagName + pathIndex);
-        }
-
-        return paths.length ? "/" + paths.join("/") : null;
-    };
-
-    if (element && element.id)
-        return '//*[@id="' + element.id + '"]';
-    else
-        return getElementTreeXPath(element);
-};
-
-PathFinder.getElementCSSPath = function(element)
-{   
-    var getElementCSSSelector = function(element)
-    {
-        if (!element || !element.localName)
-            return "null";
-
-        var label = element.localName.toLowerCase();
-        if (element.id)
-            label += "#" + element.id;
-
-        if (element.classList && element.classList.length > 0)
-            label += "." + element.classList.item(0);
-
-        return label;
-    };
-    
-    var paths = [];
-
-    for (; element && element.nodeType == 1; element = element.parentNode)
-    {
-        var selector = getElementCSSSelector(element);
-        paths.splice(0, 0, selector);
-    }
-
-    return paths.length ? paths.join(" ") : null;
-};
+  return PathFinder
+})();
